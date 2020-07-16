c9b3663c98f836123130c41bbaea7ee3f5bf8e68
sergey <sergeyr1991@gmail.com>
2020-07-16 17:56:48 +0300

    fix structure

--- tool.py
+++ tool.py
@@ -1,9 +1,9 @@
 import os
 import ifcfg
-import json
 
-if __name__ == "__main__":
-    #ifconfig = os.system("ifconfig")
+
+
+def chooseDevice():
     devicesName = []
     for name, interface in ifcfg.interfaces().items():
         # print(interface['name'])         # First IPv4 found
@@ -20,8 +20,46 @@ if __name__ == "__main__":
 
     choice = int(input());
     attDevice = devicesName[choice - 1]
-    os.system("sudo -S ifconfig " + attDevice +  " down")
+    return attDevice
+
+def APScan():
+    scanDevice = chooseDevice()
+    os.system("iwlist " + scanDevice + " scan")
+
+    # os.system("sudo -S iwlist " + scanDevice + " scan")
+
+
+
+def goMonitorMode(attDevice):
+    os.system("sudo -S ifconfig " + attDevice + " down")
     os.system("sudo -S iwconfig " + attDevice + " mode monitor")
     os.system("sudo -S ifconfig " + attDevice + " up")
-    os.system("iwconfig")
+    print(attDevice + " is now in monitor mode!")
+
+
+
+
+if __name__ == "__main__":
+
+
+    APScan()
+
+
+    # devicesName = []
+    # for name, interface in ifcfg.interfaces().items():
+    #     # print(interface['name'])         # First IPv4 found
+    #     # print(interface['inet4'])        # List of ips
+    #     # print(interface['inet6'])
+    #     # print(interface['netmask'])
+    #     # print(interface['broadcast'])
+    #     devicesName.append(interface['device'])
+    # print("Choose attacking device: ")
+    # count = 1
+    # for device in devicesName:
+    #     print(str(count) + '.' + device)
+    #     count += 1
+    #
+    # choice = int(input());
+    # attDevice = devicesName[choice - 1]
+    # goMonitorMode(attDevice)
 
