106db7f037dedc189b03a70cd0383ad6fd08cb69
sergey <sergeyr1991@gmail.com>
2020-08-16 13:53:04 +0300

    fix deAuth_defence

--- deAuth_defence.py
+++ deAuth_defence.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from scapy.all import *
 from scapy.layers.dot11 import Dot11
 
@@ -8,4 +6,6 @@ def PacketHandler( pkt ):
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0xC:
         print ("Deauth packet sniffed: %s" % (pkt.summary()))
 
-sniff(iface="mon0", prn = PacketHandler)
+
+if __name__ == "__main__":
+    sniff(iface="wlp3s0", prn = PacketHandler)
