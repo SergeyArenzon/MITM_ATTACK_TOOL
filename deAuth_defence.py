a1bc2ce1cc2e6d6288f87ff45603aa447a41314a
sergey <sergeyr1991@gmail.com>
2020-09-21 14:53:32 +0300

    add defence scenario

--- deAuth_defence.py
+++ deAuth_defence.py
@@ -1,24 +1,14 @@
 from scapy.all import *
-from scapy.layers.dot11 import Dot11
+from scapy.layers.dot11 import Dot11, RadioTap, Dot11Deauth
 
 
 def PacketHandler( pkt ):
+    deAuth_pkt_count = 0
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0xC:
-        print ("Deauth packet sniffed: %s" % (pkt.summary()))
+        deAuth_pkt_count += 1
+        if(deAuth_pkt_count >= 20):
+            print("YOU'R DEVICE IS UNDER ATTACK!!")
 
-sniff(iface="wlp3s0", prn = PacketHandler)
+sniff(iface="wlp0s20f0u1", prn = PacketHandler)
 
-#
-# count = 0
-#
-# def PacketHandler(pkt):
-#     global count
-# #    print "got pkt"
-#     if pkt.haslayer(Dot11FCS):
-#         if pkt.type == 0 and pkt.subtype == 0xc:
-# 	    count = count + 1
-#             print "Deauth packet sniffed: %d" % (count)
-#     if count >= 20:
-#         print "warning you'r AP is under deauthattack -- could lead to Evil_Twin attack or to Rouge AP call the police for more info"
-#
-# sniff(iface="wlan1", prn = PacketHandler)
+
