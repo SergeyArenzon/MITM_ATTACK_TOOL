07632b9a93d2a7c06df637a98f078503f72cadf4
sergey <sergeyr1991@gmail.com>
2020-09-21 15:13:18 +0300

    add comments

--- deAuth_defence.py
+++ deAuth_defence.py
@@ -2,6 +2,7 @@ from scapy.all import *
 from scapy.layers.dot11 import Dot11, RadioTap, Dot11Deauth
 
 
+# Sniffing for deauth pkts with monitor mode
 def PacketHandler( pkt ):
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0xC:
         print("deAuth packet sniffed!!")
